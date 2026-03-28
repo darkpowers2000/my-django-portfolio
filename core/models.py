1c9bcaa0cec50c804d55da7efcd16d251827a5f3
darkpowers2000 <youngzigs15@yahoo.com>
2026-03-28 16:54:45 -0400

    Added image support and Pillow library

--- core/models.py
+++ core/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Service(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-
+    image = models.ImageField(upload_to='services/', null=True, blank=True)
+    
     def __str__(self):
         return self.title
 
